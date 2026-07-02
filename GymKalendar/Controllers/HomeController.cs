0067328706e320becd9f79fd75e6af7bb7fb1cbc
Ivanytsia Serhii <ivserhii12@gmail.com>
2026-07-02 10:56:03 +0300

    рбновление контроллера и хтмл

--- GymKalendar/Controllers/HomeController.cs
+++ GymKalendar/Controllers/HomeController.cs
@@ -9,10 +9,79 @@ namespace GymKalendar.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly AppDbContext _db;
+
+
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+
+        [HttpPost]
+        public IActionResult IActionResult(RegisterViewModel dto)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+
+            bool isEmailTaken = _db.Users.Any(u => u.Email == dto.Email);
+            if (isEmailTaken)
+            {
+                 ModelState.AddModelError("Email", "Email is already taken.");
+                return View(dto);
+            }
+
+            User newUser = new User
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Age = dto.Age,
+                Email = dto.Email,
+                Phone = dto.Phone,
+                Password = dto.Password // In a real application, make sure to hash the password before storing it
+            };
+            _db.Users.Add(newUser);
+            _db.SaveChanges();
+
+            return RedirectToAction("Login");
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(dto);
+            }
+            var user = _db.Users.FirstOrDefault(u => (u.Email == dto.LoginInput || u.Phone == dto.LoginInput) && u.Password == dto.Password);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return View(dto);
+            }
+            return RedirectToAction("Index", "Dashboard");
+        }
+
+
+
+
+
+
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
         
     }
+
 }
